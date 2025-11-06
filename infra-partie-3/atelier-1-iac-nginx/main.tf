d9f189fe164c9be96a719328f47b3259f2136530
badr59k <khelifi.badr.1@gmail.com>
2025-11-06 09:23:12 +0100

    Atelier 1 OpenTofu: Nginx IaC

---
+++ infra-partie-3/atelier-1-iac-nginx/main.tf
@@ -0,0 +1,29 @@
+﻿terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:alpine"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  name  = "iac-nginx"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+output "nginx_url" {
+  value = "http://localhost:8080"
+}
